895ec01ccbb9068e59a70276e641bd9964868275
Emilio Prado <a01570318@itesm.mx>
2021-10-31 15:03:06 -0700

    Avance a las vistas de workshop de alumnos

---
+++ AppPrepanet/AppPrepanet/ViewControllerWorkshop.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerWorkshop.swift
+//  AppPrepanet
+//
+//  Created by Emilio Fernando Prado Chible on 31/10/21.
+//
+
+import UIKit
+
+class ViewControllerWorkshop: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AppPrepanet/AppPrepanet/Workshop.swift
@@ -0,0 +1,12 @@
+//
+//  Workshop.swift
+//  AppPrepanet
+//
+//  Created by Emilio Fernando Prado Chible on 31/10/21.
+//
+
+import UIKit
+
+class Workshop: NSObject {
+
+}
